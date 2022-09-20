d1bad6e78c31e26a623c60ae0a71383b9c1b81b6
Lew-droid <113996364+Lew-droid@users.noreply.github.com>
2022-09-20 15:04:57 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+    	
+    }
+    if (receivedNumber == 1) {
+    	
+    }
+    if (receivedNumber == 2) {
+    	
+    }
+    if (receivedNumber == 3) {
+    	
+    }
+    if (receivedNumber == 4) {
+    	
+    }
+})
+radio.setGroup(10)
+basic.forever(function () {
+	
+})
